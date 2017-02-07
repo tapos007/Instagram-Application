acd51e36d1cf7acce9cf891b4f55b3ac8afb5283
tapos <tapos.aa@gmail.com>
2017-02-08 02:49:41 +0600

    after login get all user list

--- app/src/main/java/com/example/tapos/instragramandroidapplication/UserlistActivity.java
+++ app/src/main/java/com/example/tapos/instragramandroidapplication/UserlistActivity.java
@@ -6,12 +6,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserlistActivity extends AppCompatActivity {
 
+    ListView userListView;
+    ArrayList<String> users = new ArrayList<>();
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.userlistmenu, menu);//Menu Resource, Menu
@@ -23,6 +33,7 @@ public class UserlistActivity extends AppCompatActivity {
 
         switch (item.getItemId()){
             case R.id.share:
+
                 return true;
             case R.id.logout:
 
@@ -39,5 +50,23 @@ public class UserlistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlist);
+        userListView = (ListView) findViewById(R.id.userListView);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        query.findInBackground(new FindCallback<ParseUser>() {
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e == null) {
+                    for (ParseUser user:
+                         objects) {
+                        users.add(user.getUsername());
+                    }
+                    ArrayAdapter adapter = new ArrayAdapter(UserlistActivity.this, android.R.layout.simple_list_item_1, users);
+                    userListView.setAdapter(adapter);
+                } else {
+                    // Something went wrong.
+                }
+            }
+        });
     }
 }
